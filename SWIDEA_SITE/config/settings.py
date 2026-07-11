781e2c36b1029bffa3c0f45d07fdc4666e5df58a
hyunnminn <65217613+StormtroopeR2727@users.noreply.github.com>
2026-07-11 16:36:12 +0900

    develop에서 push 2트

--- SWIDEA_SITE/config/settings.py
+++ SWIDEA_SITE/config/settings.py
@@ -20,7 +20,7 @@ MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-#hh
+#hhdd
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
