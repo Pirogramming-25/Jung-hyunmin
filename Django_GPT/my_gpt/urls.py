9d0d3824ffd72a2df666cdec70b4c6accf6dfafa
hyunnminn <65217613+StormtroopeR2727@users.noreply.github.com>
2026-07-22 23:25:39 +0900

    feat:urls 와 view 연결

---
+++ Django_GPT/my_gpt/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "my_gpt"
+
+urlpatterns = [
+    path("sentiment/", views.sentiment_view, name="sentiment"),
+    path("summarize/", views.summarize_view, name="summarize"),
+    path("moderate/", views.moderate_view, name="moderate"),
+    path("combo/", views.combo_view, name="combo"),
+]
--- Django_GPT/my_gpt/views.py
+++ Django_GPT/my_gpt/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def sentiment_view(request):
+  return HttpResponse("sentiment placeholder")
+
+def summarize_view(request):
+  return HttpResponse("summarize placeholder")
+
+def moderate_view(request):
+  return HttpResponse("moderate placeholder")
+
+def combo_view(request):
+  return HttpResponse("combo placeholder")
